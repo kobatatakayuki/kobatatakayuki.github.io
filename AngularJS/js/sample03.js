00dbd08963a789c74e91a522ddbd6070d08b1c65
小畑高之 <kobata@anagram-works.jp>
2015-10-13 11:25:05 +0900

    フィルター

---
+++ AngularJS/js/sample03.js
@@ -0,0 +1,10 @@
+var mainCtrl = function($scope) {
+	$scope.users = [
+		{"name":"kobata","score":"100"},
+		{"name":"kobata2","score":"90"},
+		{"name":"kobata3","score":"80"},
+	];
+	
+	//日付
+	$scope.today = new Date();
+}
