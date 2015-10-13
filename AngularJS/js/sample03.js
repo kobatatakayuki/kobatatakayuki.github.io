13bd3bb466804f55b8e8d273499b79e1bd4e2348
小畑高之 <kobata@anagram-works.jp>
2015-10-13 11:42:13 +0900

    微調整

--- AngularJS/js/sample03.js
+++ AngularJS/js/sample03.js
@@ -1,8 +1,8 @@
 var mainCtrl = function($scope) {
 	$scope.users = [
-		{"name":"kobata","score":"100"},
-		{"name":"kobata2","score":"90"},
-		{"name":"kobata3","score":"80"},
+		{"name":"kobata","score":100},
+		{"name":"kobata2","score":90},
+		{"name":"kobata3","score":80},
 	];
 	
 	$scope.today = new Date();
