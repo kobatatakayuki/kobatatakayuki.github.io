a1103158f88cc242efdc70c8881e600c30ed7aef
小畑高之 <kobata@anagram-works.jp>
2015-10-20 13:52:33 +0900

    no message

---
+++ AngularJS/js/sample04.js
@@ -0,0 +1,13 @@
+var mainCtrl = function($scope) {
+	$scope.users = [
+		{"name":"kobata" , "score":100},
+		{"name":"yamada" , "score":80},
+		{"name":"takeda" , "score":70},
+		{"name":"kamada" , "score":40},
+		{"name":"takenaka" , "score":50},
+		{"name":"nonaka" , "score":30},
+		{"name":"hamada" , "score":60},
+		{"name":"tanaka" , "score":90},
+	];
+}
+
